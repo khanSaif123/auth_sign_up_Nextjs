46ea547ee1c0c10209e6515c2b7da66ed2199693
khanSaif123 <khansaiflko0@gmail.com>
2024-10-28 07:12:09 +0530

    fix error

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,61 +1,55 @@
 "use client";
 
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
-
 export default function VerifyEmailPage() {
-
-    const [token, setToken] = useState("");
-    const [verified, setVerified] = useState(false);
-    const [error, setError] = useState(false);
+    const [token, setToken] = useState<string>("");
+    const [verified, setVerified] = useState<boolean>(false);
+    const [error, setError] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
 
     const verifyUserEmail = async () => {
         try {
-            await axios.post('/api/users/verifyemail', {token})
+            await axios.post("/api/users/verifyemail", { token });
             setVerified(true);
-        } catch (error:any) {
+        } catch (error) {
+            const err = error as AxiosError;
             setError(true);
-            console.log(error.reponse.data);
-            
+            setErrorMessage(err.message || "An unknown error occurred.");
+            console.log(err.response?.data);
         }
-
-    }
+    };
 
     useEffect(() => {
-        const urlToken = window.location.search.split("=")[1];
+        const urlToken = new URLSearchParams(window.location.search).get("token");
         setToken(urlToken || "");
     }, []);
 
-
     useEffect(() => {
-        if(token.length > 0) {
+        if (token.length > 0) {
             verifyUserEmail();
         }
     }, [token]);
 
-    return(
+    return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-
             <h1 className="text-4xl">Verify Email</h1>
-            <h2 className="p-2 bg-orange-500 text-black">{token ? `${token}` : "no token"}</h2>
+            <h2 className="p-2 bg-orange-500 text-black">{token ? `${token}` : "No token"}</h2>
 
             {verified && (
                 <div>
                     <h2 className="text-2xl">Email Verified</h2>
-                    <Link href="/login">
-                        Login
-                    </Link>
+                    <Link href="/login">Login</Link>
                 </div>
             )}
+
             {error && (
                 <div>
-                    <h2 className="text-2xl bg-red-500 text-black">Error</h2>
-                    
+                    <h2 className="text-2xl bg-red-500 text-black">Error: {errorMessage}</h2>
                 </div>
             )}
         </div>
-    )
-
+    );
 }
